921b103c9ed5e89c59b04656aefcd4b95f983674
Bernd Schimmer <bernd@schimmer.email>
2022-09-29 21:25:14 +0200

    cleanup and github pages test

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import Audio from "./Audio";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Settings from "./Settings";
 import "./App.css";
 
 const App = () => {
   const redordings = [1, 2, 3, 4, 5, 6];
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/settings" element={<Settings />}></Route>
-
-          <Route
-            path="/"
-            element={
-              <div>
-                {redordings.map((key) => (
-                  <Audio key={key} record={key}></Audio>
-                ))}
-              </div>
-            }
-          ></Route>
-        </Routes>
+    <div className="App">
+      <div>
+        {redordings.map((key) => (
+          <Audio key={key} record={key}></Audio>
+        ))}
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
